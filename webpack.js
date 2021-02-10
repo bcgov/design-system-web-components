d615f2758fa1ece226e951ae51fe66e5b0e2e358
Shawn Turple <shawn.turple@gov.bc.ca>
2021-02-10 08:55:45 -0800

    added es5

--- webpack.js
+++ webpack.js
@@ -15,7 +15,7 @@ const loadPlugins = isProduction => {
   if (!isProduction) {
     plugins.push(
       new WebpackShellPlugin({
-        onBuildStart: ["stencil build --prod --watch --es5"],
+        onBuildStart: ["stencil build --dev --watch "],
         dev: true
       })
     );
